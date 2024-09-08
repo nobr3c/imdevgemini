0237d8189a3b17380f13fcaf4fbff9258140509e
Nobre 81 <jnobr3c+git@gmail.com>
2024-09-08 01:42:43 -0300

    :construction: Aula 05 Imersão

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
+    let titulo = ""; 
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-        // Cria um novo elemento HTML para cada resultado
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -17,6 +35,11 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
     }
 
     // Atribui os resultados gerados à seção HTML
